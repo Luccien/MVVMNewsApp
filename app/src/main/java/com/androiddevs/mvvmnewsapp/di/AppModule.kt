59ac5e14dd07de8718eb0ab6970ac2fd27fc9eca
Your Name <you@example.com>
2021-04-27 16:55:39 +0200

    NewsApi added to AppModule but not injected yet --working

--- app/src/main/java/com/androiddevs/mvvmnewsapp/di/AppModule.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/di/AppModule.kt
@@ -1,9 +1,16 @@
 package com.androiddevs.mvvmnewsapp.di
 
+import com.androiddevs.mvvmnewsapp.api.NewsAPI
+import com.androiddevs.mvvmnewsapp.util.Constants
+import com.androiddevs.mvvmnewsapp.util.Constants.Companion.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Named
 import javax.inject.Singleton
 
@@ -25,5 +32,51 @@ object AppModule {
     @Singleton
     @Named("tex3")
     fun provideText3():String = "333333333333333oo"
+
+
+
+
+/*
+    @Singleton
+    @Provides
+    fun providePixabayApi(): PixabayAPI {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(PixabayAPI::class.java)
+    }
+
+ */
+/////////////////////////////
+    @Singleton
+    @Provides
+    fun provideNewsApi(): NewsAPI {
+    return Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .build()
+        .create(NewsAPI::class.java)
+    }
+/////////////////////////////////////
+
+/*
+    private val retrofit by lazy {
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+        Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
+
+    val api by lazy {
+        retrofit.create(NewsAPI::class.java)
+    }
+*/
 
 }
