9473a526d3413f5842dc38739224e10372451145
Your Name <you@example.com>
2021-05-05 19:20:06 +0200

    cleanup -- deleted documented out stuff 3

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
@@ -2,18 +2,13 @@ package com.androiddevs.mvvmnewsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.androiddevs.mvvmnewsapp.R
-import com.androiddevs.mvvmnewsapp.db.ArticleDatabase
-import com.androiddevs.mvvmnewsapp.repository.DefaultNewsRepository
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_news.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import javax.inject.Inject
-import javax.inject.Named
+
 
 @ExperimentalCoroutinesApi
 @AndroidEntryPoint
